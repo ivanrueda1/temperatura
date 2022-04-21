eb168f0b1a4a015c8d11f8add40de7a8fbebc758
ivanrueda1 <ivanrueda@colegioguanenta.edu.co>
2022-04-21 17:57:38 -0500

    Desarrollo: Programa para convertir grados celcius kelvin/farenheit

---
+++ Escritorio/temperatura/temperatura.py
@@ -0,0 +1,13 @@
+"""Programa No. 3: convertidor de celcius a kelvin y farenheir"""
+
+#input
+c =int (input ("digite el valor de c:  "))
+
+# processing
+
+k =c+273.15
+f =1.8*c+32
+
+#output
+
+input(str(c) + " grados en celcius, equivalen a " + str(k) + "grados kelvin y a " + str(f) + " grados farenheit ")
